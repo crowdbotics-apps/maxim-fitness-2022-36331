aa2e8f035a887833da84ab58445bb9773d291580
Jahanzaib Akhter <jahanzaib.akhter@crowdbotics.com>
2022-09-16 19:09:10 +0500

    Resolved conflicts.

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -29,8 +29,8 @@ const Navigation = props => {
         {!props.accessToken ? (
           <authStack.Screen name="MainStack" component={MainNavigator} />
         ) : (
-          //<mainStack.Screen name="AuthStack" component={AuthStackScreen} />
-          <questionStack.Screen name="QuestionStack" component={QuestionStackScreen} />
+          <mainStack.Screen name="AuthStack" component={AuthStackScreen} />
+          //<questionStack.Screen name="QuestionStack" component={QuestionStackScreen} />
         )}
       </authStack.Navigator>
     </NavigationContainer>
