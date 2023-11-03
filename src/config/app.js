bf43f0314ca4e58e9e878fba07b97cd90a041084
Jahanzaib Akhter <jahanzaib.akhter@crowdbotics.com>
2023-11-03 20:58:24 +0500

    Fixed url

--- src/config/app.js
+++ src/config/app.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://maxim-fitness-2022-36331.botics.co/api/v1';
-export const API_URL = 'https://af48-124-29-214-70.ngrok-free.app/api/v1';
+export const API_URL = 'https://maxim-fitness-2022-36331.botics.co/api/v1';
+// export const API_URL = 'https://af48-124-29-214-70.ngrok-free.app/api/v1';
