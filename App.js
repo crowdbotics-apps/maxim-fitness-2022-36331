051a5a207d8e24a91bafe8d7b3c84db18645c5da
Jahanzaib Akhter <jahanzaib.akhter@crowdbotics.com>
2023-12-05 13:41:25 +0500

    implement chat

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
-import React from 'react'
-import { ThemeProvider } from 'react-native-elements'
-import { theme } from '@options'
-import { store } from 'src/redux/store'
-import { Provider as ReduxProvider } from 'react-redux'
-import { persistStore } from 'redux-persist'
-import { PersistGate } from 'redux-persist/integration/react'
-import FlashMessage from 'react-native-flash-message'
-import Navigation from 'src/navigation'
+import React from 'react';
+import { ThemeProvider } from 'react-native-elements';
+import { theme } from '@options';
+import { store } from 'src/redux/store';
+import { Provider as ReduxProvider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import FlashMessage from 'react-native-flash-message';
+import Navigation from 'src/navigation';
+import PubNub from 'pubnub';
+import { PubNubProvider } from 'pubnub-react';
+import { PUBNUB_PUBLISH_KEY, PUBNUB_SUBSCRIBE_KEY } from '@env';
+
 const App = () => {
+  const pubnub = new PubNub({
+    publishKey: PUBNUB_PUBLISH_KEY,
+    subscribeKey: PUBNUB_SUBSCRIBE_KEY,
+    uuid: 'myUniqueUUIDDD',
+  });
+
   const persistor = persistStore(store);
   return (
-    <ReduxProvider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ThemeProvider theme={theme}>
-          <Navigation />
-          <FlashMessage />
-        </ThemeProvider>
-      </PersistGate>
-    </ReduxProvider>
+    <PubNubProvider client={pubnub}>
+      <ReduxProvider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ThemeProvider theme={theme}>
+            <Navigation />
+            <FlashMessage />
+          </ThemeProvider>
+        </PersistGate>
+      </ReduxProvider>
+    </PubNubProvider>
   );
 };
 export default App;
