c61f90b5589cdc45ab29c8dcb2991b694e3a2952
Jahanzaib Akhter <jahanzaib.akhter@crowdbotics.com>
2023-12-15 19:00:21 +0500

    fixed notification issues

--- modules/notifications/utils.js
+++ modules/notifications/utils.js
@@ -1,8 +1,8 @@
-import { Platform } from "react-native"
+import { Platform, PermissionsAndroid } from "react-native"
 import { getUniqueId, getAndroidId, getModel } from "react-native-device-info"
 import { registerDeviceInfoAPI } from "./api"
 import messaging from "@react-native-firebase/messaging"
-
+import { check, request, PERMISSIONS } from "react-native-permissions"
 /**
  * Request and generate Firebase Messaging token and store it in the DB via REST API
  * @param  {String} authToken Backend API authentication token
@@ -10,33 +10,43 @@ import messaging from "@react-native-firebase/messaging"
  * @return {Promise}
  */
 const RemotePushController = async (authToken, userID) => {
-  const authStatus = await messaging().requestPermission()
-  const ENABLED_STATUSES = [
-    messaging.AuthorizationStatus.AUTHORIZED,
-    messaging.AuthorizationStatus.PROVISIONAL
-  ]
-  const isEnabled = ENABLED_STATUSES.includes(authStatus)
+  // const authStatus = await messaging().requestPermission()
+
+  // const ENABLED_STATUSES = [
+  //   messaging.AuthorizationStatus.AUTHORIZED,
+  //   messaging.AuthorizationStatus.PROVISIONAL
+  // ]
+  // const isEnabled = ENABLED_STATUSES.includes(authStatus)
   // Checks if required permissions are allowed or not
 
-  if (isEnabled) {
-    const registrationToken = await messaging().getToken()
-    const androidId = await getAndroidId()
-    const iosId = await getUniqueId()
-    // API which registers the device details and FCM token in backend
-    await registerDeviceInfoAPI(
-      {
-        user: userID,
-        authToken: authToken,
-        registration_id: registrationToken,
-        type: Platform.OS,
-        name: getModel(),
-        active: true,
-        device_id: Platform.OS === "android" ? androidId : iosId,
-        cloud_message_type: "FCM"
-      },
-      authToken
-    )
-  }
+  // if (isEnabled) {
+  const registrationToken = await messaging().getToken()
+  const androidId = await getAndroidId()
+  const iosId = await getUniqueId()
+  // API which registers the device details and FCM token in backend
+  await registerDeviceInfoAPI(
+    {
+      user: userID,
+      authToken: authToken,
+      registration_id: registrationToken,
+      type: Platform.OS,
+      name: getModel(),
+      active: true,
+      device_id: Platform.OS === "android" ? androidId : iosId,
+      cloud_message_type: "FCM"
+    },
+    authToken
+  )
+
+  check(PERMISSIONS.ANDROID.POST_NOTIFICATIONS).then(async status => {
+    if (
+      status === PermissionsAndroid.RESULTS.DENIED ||
+      status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN
+    ) {
+      await request(PERMISSIONS.ANDROID.POST_NOTIFICATIONS)
+    }
+  })
+  // }
 }
 
 export default RemotePushController
--- src/navigation/Main.js
+++ src/navigation/Main.js
@@ -1,51 +1,51 @@
-import React from 'react';
-import { View, Image, Text, StyleSheet, Platform } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react"
+import { View, Image, Text, StyleSheet, Platform } from "react-native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { createStackNavigator } from "@react-navigation/stack"
 import {
   widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from 'react-native-responsive-screen';
-import { Images } from 'src/theme';
-import Feeds from '../screens/Feeds';
-import ViewPost from '../screens/ViewPost';
-import AddPost from '../screens/AddPost';
-import Subscription from '../screens/Subscription';
-import CreditCard from '../screens/CreditCard';
-import ProgramScreen from '../screens/ProgramScreen';
-import ExerciseScreen from '../screens/ExerciseScreen';
-import ProfileScreen from '../screens/ProfileScreen';
-import EditProfile from '../screens/EditProfile';
-import CustomCalories from '../screens/CustomCalories';
-import EditCustomCal from '../screens/CustomCalories/childScreens/EditCustomCal';
-import EditCaloriesManually from '../screens/CustomCalories/childScreens/EditCaloriesManually';
-import SearchProfile from '../screens/SearchProfile';
-import MessageScreen from '../screens/MessageScreen';
-import ChatScreen from '../screens/chatScreen';
-import NotificationScreen from '../screens/NotificationScreen';
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen"
+import { Images } from "src/theme"
+import Feeds from "../screens/Feeds"
+import ViewPost from "../screens/ViewPost"
+import AddPost from "../screens/AddPost"
+import Subscription from "../screens/Subscription"
+import CreditCard from "../screens/CreditCard"
+import ProgramScreen from "../screens/ProgramScreen"
+import ExerciseScreen from "../screens/ExerciseScreen"
+import ProfileScreen from "../screens/ProfileScreen"
+import EditProfile from "../screens/EditProfile"
+import CustomCalories from "../screens/CustomCalories"
+import EditCustomCal from "../screens/CustomCalories/childScreens/EditCustomCal"
+import EditCaloriesManually from "../screens/CustomCalories/childScreens/EditCaloriesManually"
+import SearchProfile from "../screens/SearchProfile"
+import MessageScreen from "../screens/MessageScreen"
+import ChatScreen from "../screens/chatScreen"
+import NotificationScreen from "../screens/NotificationScreen"
 
 // Custom exercise screens
-import FatLoseProgram from '../screens/CustomWorkOut/FatLoseProgram';
-import CustomExercise from '../screens/CustomWorkOut/CustomExercise';
-import AddExercise from '../screens/CustomWorkOut/AddExercise';
-import SettingScreen from '../screens/SettingScreen';
-import HomeScreen from '../screens/HomeScreen';
-import MealRegulator from '../screens/MealRegulator';
-import SelectBrand from '../screens/SelectBrand';
-import SurveyScreenMeal from '../screens/SurveyScreenMeal';
-import Alexa from '../screens/Alexa';
-import SwapExerciseScreen from '../screens/SwapExerciseScreen';
-import WorkoutCard from '../screens/WorkoutCard';
+import FatLoseProgram from "../screens/CustomWorkOut/FatLoseProgram"
+import CustomExercise from "../screens/CustomWorkOut/CustomExercise"
+import AddExercise from "../screens/CustomWorkOut/AddExercise"
+import SettingScreen from "../screens/SettingScreen"
+import HomeScreen from "../screens/HomeScreen"
+import MealRegulator from "../screens/MealRegulator"
+import SelectBrand from "../screens/SelectBrand"
+import SurveyScreenMeal from "../screens/SurveyScreenMeal"
+import Alexa from "../screens/Alexa"
+import SwapExerciseScreen from "../screens/SwapExerciseScreen"
+import WorkoutCard from "../screens/WorkoutCard"
 
-import LogFoods from '../screens/LogFoods';
-import { modules } from '@modules';
+import LogFoods from "../screens/LogFoods"
+import { modules } from "@modules"
 
-import SubscriptionScreen from '../screens/Subscription';
-const { home, profileTab, feed, nutrition, exercise } = Images;
+import SubscriptionScreen from "../screens/Subscription"
+const { home, profileTab, feed, nutrition, exercise } = Images
 
 // const Chat = modules[0].value.navigator;
-const mainStack = createStackNavigator();
-const Tab = createBottomTabNavigator();
+const mainStack = createStackNavigator()
+const Tab = createBottomTabNavigator()
 
 // @refresh reset
 const BottomNavigator = () => {
@@ -54,24 +54,26 @@ const BottomNavigator = () => {
       initialRouteName="Feed"
       screenOptions={({ route }) => ({
         tabBarStyle: {
-          shadowColor: '#000',
+          shadowColor: "#000",
           shadowOffset: {
             width: 0,
-            height: 0,
+            height: 0
           },
           shadowOpacity: 0.34,
           shadowRadius: 6.27,
 
           elevation: 10,
-          shadowColor: 'black',
-          backgroundColor: 'white',
-          height: Platform.OS === 'ios' ? hp('12%') : hp('10%'),
+          shadowColor: "black",
+          backgroundColor: "white",
+          height: Platform.OS === "ios" ? hp("12%") : hp("10%")
         },
-        tabBarButton: ['Home', 'Profile', 'Feed', 'Custom', 'FatLose'].includes(route.name)
+        tabBarButton: ["Home", "Profile", "Feed", "Custom", "FatLose"].includes(
+          route.name
+        )
           ? undefined
           : () => {
-              return null;
-            },
+              return null
+            }
       })}
     >
       <Tab.Screen
@@ -81,7 +83,12 @@ const BottomNavigator = () => {
           tabBarLabel: ({ focused }) => (
             <View style={styles.textContainer}>
               <Text style={styles.text}>Home</Text>
-              <View style={[styles.bottom, { borderBottomColor: focused ? '#0460BB' : 'white' }]} />
+              <View
+                style={[
+                  styles.bottom,
+                  { borderBottomColor: focused ? "#0460BB" : "white" }
+                ]}
+              />
             </View>
           ),
           tabBarIcon: () => (
@@ -89,7 +96,7 @@ const BottomNavigator = () => {
               <Image source={home} style={styles.iconStyle1} />
             </View>
           ),
-          header: () => null,
+          header: () => null
         }}
       />
 
@@ -100,7 +107,12 @@ const BottomNavigator = () => {
           tabBarLabel: ({ focused }) => (
             <View style={styles.textContainer}>
               <Text style={styles.text}>Profile</Text>
-              <View style={[styles.bottom, { borderBottomColor: focused ? '#0460BB' : 'white' }]} />
+              <View
+                style={[
+                  styles.bottom,
+                  { borderBottomColor: focused ? "#0460BB" : "white" }
+                ]}
+              />
             </View>
           ),
           tabBarIcon: ({ focused }) => (
@@ -108,7 +120,7 @@ const BottomNavigator = () => {
               <Image source={profileTab} style={styles.image1} />
             </View>
           ),
-          header: () => null,
+          header: () => null
         }}
       />
 
@@ -119,7 +131,12 @@ const BottomNavigator = () => {
           tabBarLabel: ({ focused }) => (
             <View style={styles.textContainer}>
               <Text style={styles.text}>Feed</Text>
-              <View style={[styles.bottom, { borderBottomColor: focused ? '#0460BB' : 'white' }]} />
+              <View
+                style={[
+                  styles.bottom,
+                  { borderBottomColor: focused ? "#0460BB" : "white" }
+                ]}
+              />
             </View>
           ),
           tabBarIcon: () => (
@@ -127,7 +144,7 @@ const BottomNavigator = () => {
               <Image source={feed} style={styles.iconStyle1} />
             </View>
           ),
-          header: () => null,
+          header: () => null
         }}
       />
 
@@ -137,8 +154,13 @@ const BottomNavigator = () => {
         options={{
           tabBarLabel: ({ focused }) => (
             <View style={styles.textContainer}>
-              <Text style={styles.text}>Nurition</Text>
-              <View style={[styles.bottom, { borderBottomColor: focused ? '#0460BB' : 'white' }]} />
+              <Text style={styles.text}>Nutrition</Text>
+              <View
+                style={[
+                  styles.bottom,
+                  { borderBottomColor: focused ? "#0460BB" : "white" }
+                ]}
+              />
             </View>
           ),
           tabBarIcon: () => (
@@ -146,7 +168,7 @@ const BottomNavigator = () => {
               <Image source={nutrition} style={styles.image2} />
             </View>
           ),
-          header: () => null,
+          header: () => null
         }}
       />
 
@@ -157,7 +179,12 @@ const BottomNavigator = () => {
           tabBarLabel: ({ focused }) => (
             <View style={styles.textContainer}>
               <Text style={styles.text}>Exercise</Text>
-              <View style={[styles.bottom, { borderBottomColor: focused ? '#0460BB' : 'white' }]} />
+              <View
+                style={[
+                  styles.bottom,
+                  { borderBottomColor: focused ? "#0460BB" : "white" }
+                ]}
+              />
             </View>
           ),
           tabBarIcon: () => (
@@ -165,13 +192,25 @@ const BottomNavigator = () => {
               <Image source={exercise} style={styles.imageStyle} />
             </View>
           ),
-          header: () => null,
+          header: () => null
         }}
       />
 
-      <Tab.Screen name="HomeScreen" component={HomeScreen} options={{ header: () => null }} />
-      <Tab.Screen name="ProfileScreen" component={ProfileScreen} options={{ header: () => null }} />
-      <Tab.Screen name="Feeds" component={Feeds} options={{ header: () => null }} />
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{ header: () => null }}
+      />
+      <Tab.Screen
+        name="ProfileScreen"
+        component={ProfileScreen}
+        options={{ header: () => null }}
+      />
+      <Tab.Screen
+        name="Feeds"
+        component={Feeds}
+        options={{ header: () => null }}
+      />
       <Tab.Screen
         name="CustomCalories"
         component={CustomCalories}
@@ -183,19 +222,19 @@ const BottomNavigator = () => {
         options={{ header: () => null }}
       />
     </Tab.Navigator>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
-  textContainer: { justifyContent: 'center', alignItems: 'center' },
-  text: { fontSize: 12, color: '#0460BB' },
+  textContainer: { justifyContent: "center", alignItems: "center" },
+  text: { fontSize: 12, color: "#0460BB" },
   bottom: { borderBottomWidth: 5, marginTop: 10, width: 33, borderRadius: 10 },
-  tabIcon: { marginTop: Platform.OS === 'ios' ? 5 : 18 },
-  imageStyle: { width: 26, height: 13, resizeMode: 'contain' },
-  iconStyle1: { width: 20, height: 19, resizeMode: 'contain' },
-  image1: { width: 20, height: 21, resizeMode: 'contain' },
-  image2: { width: 20, height: 21, resizeMode: 'contain' },
-});
+  tabIcon: { marginTop: Platform.OS === "ios" ? 5 : 18 },
+  imageStyle: { width: 26, height: 13, resizeMode: "contain" },
+  iconStyle1: { width: 20, height: 19, resizeMode: "contain" },
+  image1: { width: 20, height: 21, resizeMode: "contain" },
+  image2: { width: 20, height: 21, resizeMode: "contain" }
+})
 
 const MainNavigator = () => (
   <mainStack.Navigator
@@ -216,25 +255,37 @@ const MainNavigator = () => (
     <mainStack.Screen name="SearchProfile" component={SearchProfile} />
     <mainStack.Screen name="MessageScreen" component={MessageScreen} />
     <mainStack.Screen name="ChatScreen" component={ChatScreen} />
-    <mainStack.Screen name="NotificationScreen" component={NotificationScreen} />
+    <mainStack.Screen
+      name="NotificationScreen"
+      component={NotificationScreen}
+    />
 
     {/* <mainStack.Screen name="FatLoseProgram" component={FatLoseProgram} /> */}
     <mainStack.Screen name="CustomExercise" component={CustomExercise} />
     <mainStack.Screen name="AddExercise" component={AddExercise} />
     <mainStack.Screen name="SettingScreen" component={SettingScreen} />
     <mainStack.Screen name="EditCustomCal" component={EditCustomCal} />
-    <mainStack.Screen name="EditCaloriesManually" component={EditCaloriesManually} />
+    <mainStack.Screen
+      name="EditCaloriesManually"
+      component={EditCaloriesManually}
+    />
     <mainStack.Screen name="SurveyScreenMeal" component={SurveyScreenMeal} />
     <mainStack.Screen name="MealRegulator" component={MealRegulator} />
     <mainStack.Screen name="SelectBrand" component={SelectBrand} />
     <mainStack.Screen name="LogFoods" component={LogFoods} />
     <mainStack.Screen name="Alexa" component={Alexa} />
-    <mainStack.Screen name="SwapExerciseScreen" component={SwapExerciseScreen} />
+    <mainStack.Screen
+      name="SwapExerciseScreen"
+      component={SwapExerciseScreen}
+    />
     <mainStack.Screen name="WorkoutCard" component={WorkoutCard} />
 
-    <mainStack.Screen name="SubscriptionScreen" component={SubscriptionScreen} />
+    <mainStack.Screen
+      name="SubscriptionScreen"
+      component={SubscriptionScreen}
+    />
     {/* <mainStack.Screen name="Chat" component={Chat} /> */}
   </mainStack.Navigator>
-);
+)
 
-export default MainNavigator;
+export default MainNavigator
