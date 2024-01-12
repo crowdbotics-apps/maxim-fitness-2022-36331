14d90cff159e23f5f5c2828d9bb66dc4f63a3cf2
Jahanzaib Akhtar <jahanzaib.akhter@crowdbotics.com>
2024-01-12 17:29:00 +0500

    fix session user

--- backend/home/api/v1/viewsets.py
+++ backend/home/api/v1/viewsets.py
@@ -706,8 +706,7 @@ class SessionViewSet(ModelViewSet):
     authentication_classes = [TokenAuthentication, SessionAuthentication]
 
     def get_queryset(self):
-        session = Session.objects.filter(user=self.request.user).last()
-        return Session.objects.filter(user=self.request.user, program_id=session.program.id).order_by('date_time')
+        return Session.objects.filter(user=self.request.user).order_by('date_time')
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
