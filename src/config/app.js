5cb889a0eb1cf64c1f392bb47e9d1092bb2dea73
Jahanzaib Akhter <jahanzaib.akhter@crowdbotics.com>
2024-05-23 15:19:33 +0500

    callores issues fixed

--- src/config/app.js
+++ src/config/app.js
@@ -1,7 +1,6 @@
 export const API_URL = "https://maxim-fitness-2022-36331.botics.co/api/v1"
 export const DEEP_LINKING_API_URL = "https://maxim-fitness-2022-36331.botics.co"
-export const NUTRITIONIX_URL =
-  "https://trackapi.nutritionix.com/v2/natural/nutrients"
+export const NUTRITIONIX_URL = "https://trackapi.nutritionix.com/v2/natural/nutrients"
 export const NUTRITIONIX_SEARCH_URL = "https://trackapi.nutritionix.com/v2/search"
 
 // export const API_URL = 'https://da81-110-39-21-146.ngrok-free.app/api/v1';
